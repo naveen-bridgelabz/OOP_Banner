e6c268a0b842ee59509b2ae38fad52ef5653ca31
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-02-16 08:46:44 +0530

    First main Branch

---
+++ OOPmain.java
@@ -0,0 +1,15 @@
+//This is Main Branch
+
+/*
+This is Main Branch
+
+@version 1.0
+@author Naveen
+
+*/
+
+class OOPmain{
+	public static main(String args[]){
+		System.out.println("This is Main Branch");
+	}
+}
---
+++ test.java
@@ -0,0 +1,7 @@
+public class test{
+	public static void main(String args[]){
+
+		System.out.println(10);
+		System.out.println("Hello All..");
+	}
+}
